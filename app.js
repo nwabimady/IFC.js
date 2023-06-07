cdf838deeb8e23050c8b87f2fdd49c141d2e8243
nwabimady <nwabimady@gmail.com>
2023-06-07 23:21:56 +0200

    WIV: IFC Properties

--- app.js
+++ app.js
@@ -10,9 +10,66 @@ async function loadIfc(url) {
     await viewer.IFC.setWasmPath("./");
     const model = await viewer.IFC.loadIfcUrl(url);
     await viewer.shadowDropper.renderShadow(model.modelID);
+    viewer.context.renderer.postProduction.active = true;
 }
 
 loadIfc('./04.ifc');
 
-window.ondblclick = () => viewer.IFC.selector.pickIfcItem();
+// Properties menu
+
 window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+
+window.ondblclick = async () => {
+    const result = await viewer.IFC.selector.highlightIfcItem();
+    if (!result) return;
+    const { modelID, id } = result;
+    const props = await viewer.IFC.getProperties(modelID, id, true, false);
+    createPropertiesMenu(props);
+};
+
+const propsGUI = document.getElementById("ifc-property-menu-root");
+
+function createPropertiesMenu(properties) {
+    console.log(properties);
+
+    removeAllChildren(propsGUI);
+
+    const psets = properties.psets;
+    const mats = properties.mats;
+    const type = properties.type;
+
+    delete properties.psets;
+    delete properties.mats;
+    delete properties.type;
+
+
+    for (let key in properties) {
+        createPropertyEntry(key, properties[key]);
+    }
+
+}
+
+function createPropertyEntry(key, value) {
+    const propContainer = document.createElement("div");
+    propContainer.classList.add("ifc-property-item");
+
+    if(value === null || value === undefined) value = "undefined";
+    else if(value.value) value = value.value;
+
+    const keyElement = document.createElement("div");
+    keyElement.textContent = key;
+    propContainer.appendChild(keyElement);
+
+    const valueElement = document.createElement("div");
+    valueElement.classList.add("ifc-property-value");
+    valueElement.textContent = value;
+    propContainer.appendChild(valueElement);
+
+    propsGUI.appendChild(propContainer);
+}
+
+function removeAllChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
