e185c560d25f5310cdf4178d68d615c9e5d7f544
nwabimady <nwabimady@gmail.com>
2023-06-08 00:46:13 +0200

    geometry-preprocessing-export

--- app.js
+++ app.js
@@ -1,32 +1,68 @@
-import { Color, LineBasicMaterial, MeshBasicMaterial } from 'three';
+import { Color } from 'three';
 import { IfcViewerAPI } from 'web-ifc-viewer';
+import {IFCWALL,
+    IFCWALLSTANDARDCASE,
+    IFCSLAB,
+    IFCWINDOW,
+    IFCMEMBER,
+    IFCPLATE,
+    IFCCURTAINWALL,
+    IFCFLOWFITTING,
+    IFCFLOWSEGMENT,
+    IFCFLOWTERMINAL,
+    IFCBUILDINGELEMENTPROXY,
+    IFCDOOR} from 'web-ifc';
 
 const container = document.getElementById('viewer-container');
 const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xf0faff) });
 viewer.grid.setGrid();
 viewer.axes.setAxes();
-init();
+viewer.IFC.setWasmPath("./");
 
-async function init() {
-    await viewer.IFC.setWasmPath('./');
-    const model = await viewer.IFC.loadIfcUrl('./04.ifc');
+const input = document.getElementById('file-input');
+input.onchange = loadIfc;
 
+async function loadIfc(event) {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
 
-	// Serialize properties
-	const result = await viewer.IFC.properties.serializeAllProperties(model, undefined, (current, total) => {
-		const progress = current / total;
-		const formatted = Math.trunc(progress * 100);
-		console.log(formatted + '%');
-	});
+    // Export to glTF and JSON
+    const result = await viewer.GLTF.exportIfcFileAsGltf({
+        ifcFileUrl: url,
+        categories: {
+            walls: [IFCWALL, IFCWALLSTANDARDCASE],
+            slabs: [IFCSLAB],
+            windows: [IFCWINDOW],
+            curtainwalls: [IFCMEMBER, IFCPLATE, IFCCURTAINWALL],
+            doors: [IFCDOOR],
+            pipes: [IFCFLOWFITTING, IFCFLOWSEGMENT, IFCFLOWTERMINAL],
+            undefined: [IFCBUILDINGELEMENTPROXY]
+        },
+        getProperties: true
+    });
 
-	// Download the properties as JSON file
-	const file = new File(result, 'properties');
+    // Download result
+    const link = document.createElement('a');
+    document.body.appendChild(link);
 
-	const link = document.createElement('a');
-	document.body.appendChild(link);
-	link.href = URL.createObjectURL(file);
-	link.download = 'properties.json';
-	link.click();
-	link.remove();
+    for(const categoryName in result.gltf) {
+        const category = result.gltf[categoryName];
+        for(const levelName in category) {
+            const file = category[levelName].file;
+            if(file) {
+                link.download = `${file.name}_${categoryName}_${levelName}.gltf`;
+                link.href = URL.createObjectURL(file);
+                link.click();
+            }
+        }
+    }
 
+    for(let jsonFile of result.json) {
+        link.download = `${jsonFile.name}.json`;
+        link.href = URL.createObjectURL(jsonFile);
+        link.click();
+    }
+
+    link.remove();
 }
+
