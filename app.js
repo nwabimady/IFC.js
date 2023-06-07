2ccc38a22781375d04e65732fccafa3ba9e9c1a0
nwabimady <nwabimady@gmail.com>
2023-06-07 23:58:40 +0200

    Visibility Multi

--- app.js
+++ app.js
@@ -1,81 +1,95 @@
+import {
+	IFCWALLSTANDARDCASE,
+	IFCSLAB,
+	IFCDOOR,
+	IFCWINDOW,
+	IFCFURNISHINGELEMENT,
+	IFCMEMBER,
+	IFCPLATE,
+} from 'web-ifc';
+
+import { Color } from 'three';
 import { IfcViewerAPI } from 'web-ifc-viewer';
 
-main();
+const container = document.getElementById('viewer-container');
+const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xf0faff) });
+viewer.grid.setGrid();
+viewer.axes.setAxes();
 
-async function main() {
-	const viewer = await setupScene();
-	const ifcModel = await viewer.IFC.loadIfcUrl('./04.ifc');
-	const allIDs = getAllIds(ifcModel);
-	const subset = getWholeSubset(viewer, ifcModel, allIDs);
-	replaceOriginalModelBySubset(viewer, ifcModel, subset);
-	setupEvents(viewer, allIDs);
+async function loadIfc(url) {
+	await viewer.IFC.setWasmPath("./");
+	const model = await viewer.IFC.loadIfcUrl(url);
+	model.removeFromParent();
+	await viewer.shadowDropper.renderShadow(model.modelID);
+
+	await setupAllCategories();
 }
 
-function setupEvents(viewer, allIDs) {
-	window.ondblclick = () => hideClickedItem(viewer);
-	window.onkeydown = (event) => {
-		if (event.code === 'Escape') {
-			showAllItems(viewer, allIDs);
-		}
-	};
+loadIfc('./04.ifc');
+
+const scene = viewer.context.getScene();
+
+// List of categories names
+const categories = {
+	IFCWALLSTANDARDCASE,
+	IFCSLAB,
+	IFCFURNISHINGELEMENT,
+	IFCDOOR,
+	IFCWINDOW,
+	IFCPLATE,
+	IFCMEMBER,
+};
+
+// Gets the name of a category
+function getName(category) {
+	const names = Object.keys(categories);
+	return names.find(name => categories[name] === category);
 }
 
-function getAllIds(ifcModel) {
-	return Array.from(
-		new Set(ifcModel.geometry.attributes.expressID.array),
-	);
+// Gets all the items of a category
+async function getAll(category) {
+	return viewer.IFC.loader.ifcManager.getAllItemsOfType(0, category, false);
 }
 
-function replaceOriginalModelBySubset(viewer, ifcModel, subset) {
-	const items = viewer.context.items;
-
-	items.pickableIfcModels = items.pickableIfcModels.filter(model => model !== ifcModel);
-	items.ifcModels = items.ifcModels.filter(model => model !== ifcModel);
-	ifcModel.removeFromParent();
-
-	items.ifcModels.push(subset);
-	items.pickableIfcModels.push(subset);
-}
-
-function getWholeSubset(viewer, ifcModel, allIDs) {
+// Creates a new subset containing all elements of a category
+async function newSubsetOfType(category) {
+	const ids = await getAll(category);
 	return viewer.IFC.loader.ifcManager.createSubset({
-		modelID: ifcModel.modelID,
-		ids: allIDs,
-		applyBVH: true,
-		scene: ifcModel.parent,
-		removePrevious: true,
-		customID: 'full-model-subset',
-	});
-}
-
-async function setupScene() {
-	const container = document.getElementById('viewer-container');
-	const viewer = new IfcViewerAPI({ container });
-	viewer.grid.setGrid();
-	viewer.axes.setAxes();
-	await viewer.IFC.setWasmPath('./');
-	return viewer;
-}
-
-function showAllItems(viewer, ids) {
-	viewer.IFC.loader.ifcManager.createSubset({
 		modelID: 0,
+		scene,
 		ids,
-		removePrevious: false,
-		applyBVH: true,
-		customID: 'full-model-subset',
+		removePrevious: true,
+		customID: category.toString(),
 	});
 }
 
-function hideClickedItem(viewer) {
-	const result = viewer.context.castRayIfc();
-	if (!result) return;
-	const manager = viewer.IFC.loader.ifcManager;
-	const id = manager.getExpressId(result.object.geometry, result.faceIndex);
-	viewer.IFC.loader.ifcManager.removeFromSubset(
-		0,
-		[id],
-		'full-model-subset',
-	);
+// Stores the created subsets
+const subsets = {};
+
+async function setupAllCategories() {
+	const allCategories = Object.values(categories);
+	for (let i = 0; i < allCategories.length; i++) {
+		const category = allCategories[i];
+		await setupCategory(category);
+	}
 }
+
+// Creates a new subset and configures the checkbox
+async function setupCategory(category) {
+	subsets[category] = await newSubsetOfType(category);
+	setupCheckBox(category);
+}
+
+// Sets up the checkbox event to hide / show elements
+function setupCheckBox(category) {
+	const name = getName(category);
+	const checkBox = document.getElementById(name);
+	checkBox.addEventListener('change', (event) => {
+		const checked = event.target.checked;
+		const subset = subsets[category];
+		if (checked) scene.add(subset);
+		else subset.removeFromParent();
+	});
+}
+
 
