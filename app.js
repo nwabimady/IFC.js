2e270bf5f72e33204bf3d6d5007db732233c640d
nwabimady <nwabimady@gmail.com>
2023-06-08 00:05:23 +0200

    Multithreading

--- app.js
+++ app.js
@@ -1,95 +1,105 @@
 import {
-	IFCWALLSTANDARDCASE,
-	IFCSLAB,
-	IFCDOOR,
-	IFCWINDOW,
-	IFCFURNISHINGELEMENT,
-	IFCMEMBER,
-	IFCPLATE,
-} from 'web-ifc';
+	AmbientLight,
+	AxesHelper,
+	DirectionalLight,
+	GridHelper,
+	PerspectiveCamera,
+	Scene,
+	WebGLRenderer,
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { IFCLoader } from 'web-ifc-three/IFCLoader';
 
-import { Color } from 'three';
-import { IfcViewerAPI } from 'web-ifc-viewer';
+//Creates the Three.js scene
+const scene = new Scene();
 
-const container = document.getElementById('viewer-container');
-const viewer = new IfcViewerAPI({ container, backgroundColor: new Color(0xf0faff) });
-viewer.grid.setGrid();
-viewer.axes.setAxes();
-
-async function loadIfc(url) {
-	await viewer.IFC.setWasmPath("./");
-	const model = await viewer.IFC.loadIfcUrl(url);
-	model.removeFromParent();
-	await viewer.shadowDropper.renderShadow(model.modelID);
-
-	await setupAllCategories();
-}
-
-loadIfc('./04.ifc');
-
-const scene = viewer.context.getScene();
-
-// List of categories names
-const categories = {
-	IFCWALLSTANDARDCASE,
-	IFCSLAB,
-	IFCFURNISHINGELEMENT,
-	IFCDOOR,
-	IFCWINDOW,
-	IFCPLATE,
-	IFCMEMBER,
+//Object to store the size of the viewport
+const size = {
+	width: window.innerWidth,
+	height: window.innerHeight,
 };
 
-// Gets the name of a category
-function getName(category) {
-	const names = Object.keys(categories);
-	return names.find(name => categories[name] === category);
+//Creates the camera (point of view of the user)
+const camera = new PerspectiveCamera(75, size.width / size.height);
+camera.position.z = 15;
+camera.position.y = 13;
+camera.position.x = 8;
+
+//Creates the lights of the scene
+const lightColor = 0xf0faff;
+
+const ambientLight = new AmbientLight(lightColor, 0.5);
+scene.add(ambientLight);
+
+const directionalLight = new DirectionalLight(lightColor, 1);
+directionalLight.position.set(0, 10, 0);
+directionalLight.target.position.set(-5, 0, 0);
+scene.add(directionalLight);
+scene.add(directionalLight.target);
+
+//Sets up the renderer, fetching the canvas of the HTML
+const threeCanvas = document.getElementById('three-canvas');
+const renderer = new WebGLRenderer({ canvas: threeCanvas, alpha: true });
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+//Creates grids and axes in the scene
+const grid = new GridHelper(50, 30);
+scene.add(grid);
+
+const axes = new AxesHelper();
+axes.material.depthTest = false;
+axes.renderOrder = 1;
+scene.add(axes);
+
+//Creates the orbit controls (to navigate the scene)
+const controls = new OrbitControls(camera, threeCanvas);
+controls.enableDamping = true;
+controls.target.set(-2, 0, 0);
+
+//Animation loop
+const animate = () => {
+	controls.update();
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate);
+};
+
+animate();
+
+//Adjust the viewport to the size of the browser
+window.addEventListener('resize', () => {
+	(size.width = window.innerWidth), (size.height = window.innerHeight);
+	camera.aspect = size.width / size.height;
+	camera.updateProjectionMatrix();
+	renderer.setSize(size.width, size.height);
+});
+
+//Sets up the IFC loading
+const ifcLoader = new IFCLoader();
+
+const input = document.getElementById('file-input');
+input.addEventListener(
+	'change',
+	(changed) => {
+		const ifcURL = URL.createObjectURL(changed.target.files[0]);
+		ifcLoader.load(ifcURL, (ifcModel) => scene.add(ifcModel));
+	},
+	false,
+);
+
+async function setUpMultiThreading() {
+	await ifcLoader.ifcManager.useWebWorkers(true, 'IFCWorker.js');
+	await ifcLoader.ifcManager.setWasmPath('./');
 }
 
-// Gets all the items of a category
-async function getAll(category) {
-	return viewer.IFC.loader.ifcManager.getAllItemsOfType(0, category, false);
-}
+setUpMultiThreading();
 
-// Creates a new subset containing all elements of a category
-async function newSubsetOfType(category) {
-	const ids = await getAll(category);
-	return viewer.IFC.loader.ifcManager.createSubset({
-		modelID: 0,
-		scene,
-		ids,
-		removePrevious: true,
-		customID: category.toString(),
+function setupProgressNotification() {
+	const progressText = document.getElementById('progress-text');
+	ifcLoader.ifcManager.setOnProgress((event) => {
+		const result = Math.trunc(event.loaded / event.total * 100);
+		progressText.innerText = result.toString();
 	});
 }
 
-// Stores the created subsets
-const subsets = {};
-
-async function setupAllCategories() {
-	const allCategories = Object.values(categories);
-	for (let i = 0; i < allCategories.length; i++) {
-		const category = allCategories[i];
-		await setupCategory(category);
-	}
-}
-
-// Creates a new subset and configures the checkbox
-async function setupCategory(category) {
-	subsets[category] = await newSubsetOfType(category);
-	setupCheckBox(category);
-}
-
-// Sets up the checkbox event to hide / show elements
-function setupCheckBox(category) {
-	const name = getName(category);
-	const checkBox = document.getElementById(name);
-	checkBox.addEventListener('change', (event) => {
-		const checked = event.target.checked;
-		const subset = subsets[category];
-		if (checked) scene.add(subset);
-		else subset.removeFromParent();
-	});
-}
-
-
+setupProgressNotification();
